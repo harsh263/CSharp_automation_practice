afc8c7569d9f0069a7f08cae64320ed149732fac
Harsh shah <harhs.shah263@gmail.com>
2022-02-09 23:47:11 +0530

    Added Claaa library test case and Select test case

--- CSharp_unit_test_practice/CSharp_unit_test_practice/UnitTest1.cs
+++ CSharp_unit_test_practice/CSharp_unit_test_practice/UnitTest1.cs
@@ -5,6 +5,11 @@ using OpenQA.Selenium.Firefox;
 using System.Collections.Generic;
 using System.Threading;
 using WebDriverManager.DriverConfigs.Impl;
+using Class_library_practice_selenium;
+using System.Collections;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Linq;
 
 namespace CSharp_unit_test_practice
 {
@@ -29,7 +34,8 @@ namespace CSharp_unit_test_practice
             driver.FindElement(By.CssSelector(".close-dialog")).Click();
             driver.FindElement(By.CssSelector(".cc-btn")).Click();
 
-            IList<IWebElement> cards = driver.FindElements(By.CssSelector("div > mat-card"));
+            IList<IWebElement> cards = new List<IWebElement>();
+            cards = driver.FindElements(By.CssSelector("div > mat-card"));
 
             foreach (IWebElement product_card in cards)
             {
@@ -44,6 +50,53 @@ namespace CSharp_unit_test_practice
                     Thread.Sleep(1);
                 }
                 
+            }
+        }
+
+        [TestMethod]
+        public void NavigationTest()
+        {
+            //TestCase_from_Class_Library test_navigation = new TestCase_from_Class_Library();
+            //bool nav_success = test_navigation.Test_Navigation(driver);
+        }
+
+        [TestMethod]
+        public void Test_Select_Element()
+        {
+            // Launch the URL
+            driver.Url = "https://demoqa.com/select-menu";
+
+            SelectElement oSelection = new SelectElement(driver.FindElement(By.Id("oldSelectMenu")));
+
+            //  Select option 'Green' (Use selectByIndex)
+            oSelection.SelectByText("Green");
+
+            // Using sleep command so that changes can be notice
+            Thread.Sleep(2000);
+
+            // Select option 'White' now (Use selectByVisibleText)
+            oSelection.SelectByIndex(6);
+            Thread.Sleep(2000);
+
+            // Print all the options for the selected drop down and select one option of your choice
+            // Get the size of the Select element
+            IList<IWebElement> oSize = oSelection.Options;
+
+            int iListSize = oSize.Count;
+            // Setting up the loop to print all the options
+            for (int i = 0; i < iListSize; i++)
+            {
+                // Storing the value of the option	
+                String sValue = oSelection.Options.ElementAt(i).Text;
+                // Printing the stored value
+                Console.WriteLine("Value of the Select item is : " + sValue);
+
+                // Putting a check on each option that if any of the option is equal to 'Black" then select it 
+                if (sValue.Equals("Black"))
+                {
+                    oSelection.SelectByIndex(i);
+                    break;
+                }
             }
         }
 
