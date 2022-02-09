c53075d6d1988d63ee1543a9693856a51d6b95bb
Harsh shah <harhs.shah263@gmail.com>
2022-02-09 23:47:45 +0530

    Added Class library test case and Select test case

---
+++ CSharp_unit_test_practice/Class_library_practice_selenium/TestCaseFromClassLib.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+
+namespace Class_library_practice_selenium
+{
+    public class TestCase_from_Class_Library
+    {
+        public bool Test_Navigation(IWebDriver driver)
+        {
+            driver.FindElement(By.Id("")).Click();
+            Thread.Sleep(2000);
+            if(driver.Url == "")
+            {
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+    }
+}
